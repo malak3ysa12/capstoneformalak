8ed841d2ab7ab200665c5d74adc3f6ee65633a58
Malak Essa <53011237+malak3ysa12@users.noreply.github.com>
2021-08-12 22:43:57 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,7 +9,7 @@ dockerpath=malak3ysa/capstineformalak
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run latest --image=$dockerpath --port=6000
+kubectl run latest --image=$dockerpath:1.0 --port=6000
 
 
 # Step 3:
